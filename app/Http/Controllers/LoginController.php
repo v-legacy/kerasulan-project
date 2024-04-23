40ec5016c493b94582a8de6e4643079894e4c163
rp.quraisy@gmail.com <rp.quraisy@gmail.com>
2024-04-23 09:36:13 +0700

    feat: Auth login user & add username field to users table

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,13 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class LoginController extends Controller
 {
     public function showLoginForm()
     {
-
         return view('login.index');
+    }
+
+    public function login(Request $request)
+    {
+
+        $data = $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        $user  = User::where('username', $request->username)->first();
+        try {
+
+            if (auth()->attempt($request->only('username', 'password'))) {
+                return redirect()->route('dashboard.index')->with('success', 'Login Berhasil');
+            } else {
+                return back()->with('failed', 'Login Gagal');
+            }
+        } catch (\Exception $th) {
+
+            return back()->with('failed', $th->getMessage());
+        }
     }
 }
